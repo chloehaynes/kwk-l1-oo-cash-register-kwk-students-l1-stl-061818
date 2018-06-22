b9e8ae1f52c49b46aa79f0c39794892b9257267b
chloehaynes <19chaynes@nerinxhs.org>
2018-06-22 20:10:29 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -8,3 +8,35 @@
 
 
 # puts Amazon
+
+
+
+
+
+
+ 
+cart_item_prices = [12.43, 19.99, 3.49, 75.00]
+big_ticket_items = []
+ 
+cart_item_prices.each do |price|
+  if price >= 15 
+    big_ticket_items << price 
+  end
+end
+ 
+puts big_ticket_items
+ prints out items that are over $15 in the array #cart_item_prices
+ 
+
+
+
+cart_item_prices = [12.43, 19.99, 3.49, 75.00]
+ 
+total = 0 
+ 
+cart_item_prices.each do |price|
+  total += price 
+end
+ 
+puts total 
+ adds all the cart item prices together and stores them in a variable called total
